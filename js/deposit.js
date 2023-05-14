1211b9e1535400f36952b3f9692f0495bb7d9d7c
yousufislamme <online.okk@gmail.com>
2023-05-14 08:54:03 +0600

    just add

--- js/deposit.js
+++ js/deposit.js
@@ -3,19 +3,26 @@
 document.getElementById('deposit__submit').addEventListener('click', function () {
    const depositAmountSt = document.getElementById('depositAmount');
    const depositInputSt = document.getElementById('deposit__input');
+   const currentBalanceSt = document.getElementById('balance__total');
 
    // string to number
    const previewsDepositTotal = parseFloat(depositAmountSt.innerText);
    const depositInput = parseFloat(depositInputSt.value);
-
+   const currentBalance = parseFloat(currentBalanceSt.innerText);
 
    // calculate 
    const totalDepositAmount = previewsDepositTotal + depositInput;
    // 
-   const depositTotalCalculation = totalDepositAmount;
+   const depositTotalCalculation = currentBalance + previewsDepositTotal;
+
+
 
    // show deposit amount into display...
-   depositAmountSt.innerText = depositTotalCalculation;
+   currentBalanceSt.innerText = depositTotalCalculation;
+   depositAmountSt.innerText = totalDepositAmount;
+
+   console.log(depositTotalCalculation);
+   console.log(totalDepositAmount);
 
    // clear deposit input
    depositInputSt.value = ''
---
+++ js/withdraw.js
@@ -0,0 +1,3 @@
+document.getElementById('').addEventListener('click', function () {
+
+});
