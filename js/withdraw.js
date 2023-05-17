5243c29b7942741eb4ef27d4980854e6c64dcf8a
yousufislamme <online.okk@gmail.com>
2023-05-17 08:52:26 +0600

    just upload

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,3 +1,9 @@
-document.getElementById('').addEventListener('click', function () {
+document.getElementById('withdraw__button').addEventListener('click', function () {
+   const withdrawInput = document.getElementById('withdraw__input');
+
+
+   const withdrawValue = praseFloat(withdrawInput.value);
+
+   console.log(withdrawValue);
 
 });
